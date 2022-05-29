fe830fc443b819ad2ddf63265beb512f264011ed
anaysoul <soulyana_lakew@outlook.com>
2022-05-29 12:44:57 -0400

    tracking input states

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
   password = '';
 
   onButtonClick() {
     this.password = 'MY PASSWORD';
+    console.log(this.includeLetters);
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
   }
 }
